f035bf2b65a17d9b21b5389f3703735c49904032
opengeeksLab <drudeveldvurech@gmail.com>
2019-05-28 15:35:42 +0300

    env init

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,5 @@
-const environment = process.env.NODE_ENV || 'development';
-
 export default {
-  BASE_URL: 'http://127.0.0.1:8001',
+  BASE_URL: process.env.GATSBY_BASE_URL || 'http://127.0.0.1:8001',
   isServer: typeof window === 'undefined',
-  environment,
+  environment: process.env.NODE_ENV || 'development',
 };
